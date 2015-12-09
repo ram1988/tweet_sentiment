cc7797c98f554d4e44277ce3c6d56ac7acac5f8d
ram1988 <ramnarayan.cse@gmail.com>
2015-12-09 22:50:10 +0800

    removed credentials

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-twitter_consumer_key='cbvtAvkjGi8P1rI5YzQxRdT7I'
-twitter_consumer_secret='L70corH4XMiow28j9jQyTY00Qg5bPaVbgzV7Z276d9NnNluovl'
-alchemy_apikey='a0352834a18baa938217b8f2bc7eac2a4f8691a3'
+twitter_consumer_key=''
+twitter_consumer_secret=''
+alchemy_apikey=''
